70466a9c2d610debd8fb9ba12a9e9542c6f56494
Carlos Rodrigues <cadu.mrod@gmail.com>
2024-08-18 16:48:29 -0300

    Executavel e alteracoes no setup.py

--- setup.py
+++ setup.py
@@ -2,25 +2,37 @@ import sys
 import os
 from cx_Freeze import setup, Executable
 
-files = [('assets/icons/', 'assets/icons/'),
-         ('database/', 'database/'),
-         'app.py']
+
+files = [
+    ('assets/icons/', 'assets/icons/'),
+    ('database/', 'database/')
+]
+
 
 base = None
 if sys.platform == 'win32':
     base = 'Win32GUI'
 
-config = Executable(
-    script='app.py',
-    icon='assets/icons/python.ico',
-    base=base
-)
+
+executables = [
+    Executable(
+        script='app.py',
+        base=base,
+        icon='assets/icons/python.ico'
+    )
+]
+
 
 setup(
-    name='Sistema de Login',
+    name='Sistema de login',
     version='1.0',
-    description='Aplicação de sistema de login',
+    description='Sistema de login desenvolvido em python e SQLite3',
     author='Carlos Rodrigues',
-    options={'build_exe': {'include_files': files}},
-    executables=[config]
+    options={
+        'build_exe': {
+            'packages': ['os', 'customtkinter', 'sqlite3', 'bcrypt'],
+            'include_files': files,
+        }
+    },
+    executables=executables
 )
